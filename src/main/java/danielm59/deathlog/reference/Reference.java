c18754c5a545d31e49261193ed3a07bb1de29f0e
danielm59 <danielm59@gmail.com>
2015-08-23 11:52:04 +0100

    Version will now match up

--- src/main/java/danielm59/deathlog/reference/Reference.java
+++ src/main/java/danielm59/deathlog/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 
 	public static final String MODID = "deathlog";
 	public static final String MODNAME = "Death Log";
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "@VERSION@";
 	public static final String CPROXY = "danielm59.deathlog.proxy.ClientProxy";
 	public static final String SPROXY = "danielm59.deathlog.proxy.ServerProxy";
 	public static final String GUIFACTORY = "danielm59.deathlog.client.gui.GuiFactory";
